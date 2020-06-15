c54d60f118967d061d3680fa9e087adafb35f8c4
gunes10ur <gunesonur@gmail.com>
2020-06-15 14:15:39 +0300

    better readability

--- projecteuler2.lisp
+++ projecteuler2.lisp
@@ -5,14 +5,17 @@
 
 (loop
   (setq term_new (+ term_1 term_2))
-  (when (> term_new 4000000) (return 0))
+  (when (> term_new 4000000)
+    (return 0))
   (cond
     ((= 0 (mod term_new 2))
      (setq sum_even_valued_fibonacci (+ sum_even_valued_fibonacci term_new)))
     )
   (cond
-    ((> term_1 term_2)(setq term_2 term_new))
-    ((> term_2 term_1)(setq term_1 term_new))
+    ((> term_1 term_2)
+     (setq term_2 term_new))
+    ((> term_2 term_1)
+     (setq term_1 term_new))
     )
   )
 
--- projecteuler3.lisp
+++ projecteuler3.lisp
@@ -8,9 +8,12 @@
   (setq i 2)
   (loop
     (cond
-      ((= x_number 1)(return NIL))
-      ((> i sqrt_x_number)(return T))
-      ((= 0 (mod x_number i))(return NIL))
+      ((= x_number 1)
+       (return NIL))
+      ((> i sqrt_x_number)
+       (return T))
+      ((= 0 (mod x_number i))
+       (return NIL))
       )
     (setq i (+ i 1))
     )
@@ -19,8 +22,10 @@
 (setq j 2)
 (loop
   (setq j (+ j 1))
-  (if (and (= 0 (mod a_number j)) (is_prime j)) (setq largest_prime_number_found j))
-  (if (> j sqrt_a_number) (return NIL))
+  (if (and (= 0 (mod a_number j)) (is_prime j))
+    (setq largest_prime_number_found j))
+  (if (> j sqrt_a_number)
+    (return NIL))
   )
 
 (print largest_prime_number_found)
